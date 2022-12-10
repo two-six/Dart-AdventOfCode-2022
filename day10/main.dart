b8792a708fdabda0c1251bee70972e0e1cf58d31
two-six <twopsix@gmail.com>
2022-12-10 23:52:53 +0100

    day10

--- day10/main.dart
+++ day10/main.dart
@@ -1,91 +1,63 @@
 import 'dart:io';
 import 'dart:async';
 
-const List<int> cycles = [20, 60, 100, 140, 180, 220];
-
-Future<void> main() async {
-  String input = await File('input.txt').readAsString();
-  Iterable<List<String>> instructions =
-      input.split('\n').map((e) => e.split(' '));
-  print('Silver: ${silver(instructions.toList())}');
-  print('Gold: \n${gold(instructions.toList())}');
-}
-
-int silver(List<List<String>> instructions) {
-  int cycle = 0;
-  int x = 1;
-  List<int> results = [];
-  instructions.forEach((line) {
-    switch (line.first) {
-      case 'noop':
-        {
-          cycle++;
-          if (check(x, cycle) is int) {
-            results.add(x * cycle);
-          }
-        }
-        break;
-      case 'addx':
-        {
-          cycle++;
-          if (check(x, cycle) is int) {
-            results.add(x * cycle);
-          }
-          cycle++;
-          if (check(x, cycle) is int) {
-            results.add(x * cycle);
-          }
-          x += int.parse(line[1]);
-        }
-    }
-  });
-  return results.reduce((prev, next) => prev + next);
-}
-
-int? check(int x, cycle) {
-  for (int c in cycles) {
-    if (c == cycle) {
-      return x;
-    }
-  }
-  return null;
-}
-
-String gold(List<List<String>> instructions) {
+class CPU {
+  List<int> _cycles = [20, 60, 100, 140, 180, 220];
+  List<List<String>> crt =
+      List.generate(6, (_) => List.generate(40, (_) => ' '));
   int x = 1;
   String currentInstruction = '';
   int currentInstructionValue = 0;
   int currentInstructionCyclesLeft = 0;
   int instructionLine = 0;
-  List<List<String>> crt =
-      List.generate(6, (_) => List.generate(40, (_) => '.'));
-  for (List<String> line in crt) {
+  int currentCycle = 0;
+  List<int> signals = [];
+  List<List<String>> instructions = [];
+
+  void getNextInstruction() {
+    if (currentInstruction == 'addx') x += currentInstructionValue;
+    currentInstruction = instructions[instructionLine].first;
+    if (currentInstruction == 'addx') {
+      currentInstructionCyclesLeft = 2;
+      currentInstructionValue = int.parse(instructions[instructionLine].last);
+    } else
+      currentInstructionCyclesLeft = 1;
+    instructionLine++;
+  }
+
+  void checkSignal() {
+    _cycles.forEach((cycle) {
+      if (cycle == currentCycle) signals.add(x * currentCycle);
+    });
+  }
+
+  CPU(List<List<String>> instructions) {
+    this.instructions = instructions;
+  }
+}
+
+Future<void> main() async {
+  String input = await File('input.txt').readAsString();
+  Iterable<List<String>> instructions =
+      input.split('\n').map((e) => e.split(' '));
+  var results = solution(instructions.toList());
+  print('Silver: ${results['silver']}');
+  print('Gold: \n${results['gold']}');
+}
+
+Map<String, String> solution(List<List<String>> instructions) {
+  CPU cpu = CPU(instructions);
+  for (List<String> line in cpu.crt) {
     for (int i = 0; i < line.length; i++) {
-      if (currentInstructionCyclesLeft == 0) {
-        if (currentInstruction == 'addx') {
-          x += currentInstructionValue;
-        }
-        currentInstruction = instructions[instructionLine].first;
-        switch (currentInstruction) {
-          case 'addx':
-            {
-              currentInstructionCyclesLeft = 2;
-              currentInstructionValue =
-                  int.parse(instructions[instructionLine].last);
-            }
-            break;
-          case 'noop':
-            {
-              currentInstructionCyclesLeft = 1;
-            }
-        }
-        instructionLine++;
-      }
-      if ((i - x).abs() <= 1) {
-        line[i] = '#';
-      }
-      currentInstructionCyclesLeft--;
+      cpu.checkSignal();
+      cpu.currentCycle++;
+      if (cpu.currentInstructionCyclesLeft == 0) cpu.getNextInstruction();
+      if ((i - cpu.x).abs() <= 1) line[i] = '#';
+      cpu.currentInstructionCyclesLeft--;
     }
   }
-  return crt.map((line) => line.join()).join('\n');
+  return {
+    'silver': '${cpu.signals.reduce((prev, next) => prev + next)}',
+    'gold': cpu.crt.map((line) => line.join()).join('\n'),
+  };
 }
